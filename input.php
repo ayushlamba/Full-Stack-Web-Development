c6039ed74be5803ab1dcfedca822f5db4431a43f
ayushlamba1998 <60417111+ayushlamba1998@users.noreply.github.com>
2020-03-19 13:45:29 +0530

    taking data of student

---
+++ input.php
@@ -0,0 +1,19 @@
+<html>
+<head><title>Input Data</title></head>
+
+<body>
+  <center>  <form action="addRecord.php" method="post">
+      Enrollment:- <input name="Enrollment" type="text" placeholder="Enter Enrollment no."><br>
+
+      Name: - <input name="Name" type="text" placeholder="Enter Name"><br>
+
+      Age: - <input name="Age" type="text" placeholder="Enter Age"><br>
+
+      Course: - <input name="Course" type="text" placeholder="Enter course"><br>
+      Branch:-<input type="text" name="Branch" placeholder="Enter Branch"><br>
+      Contact:-<input type="text" name="Contact" placeholder="Enter Contact"><br>
+      Email:-<input type="email" name="Email" placeholder="Enter email"><br>
+      <input name="submit" type="submit">
+    </form></center>
+</body>
+</html>
